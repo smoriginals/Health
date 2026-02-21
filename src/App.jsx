ba7a75e4eb40ec9dbc18c3dfb1a0551d1ab31d69
smoriginals <smoriginals@users.noreply.github.com>
2026-02-21 12:25:05 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
-import Navbar from './components/Navbar';
-import Herobar from './components/Herobar';
-import Quotes from './pages/Quotes';
-import Hospitals from './pages/Hospitals';
-import Solutions from './pages/Solutions';
-import HealthGPT from './pages/HealthGPT';
-import Specialties from './pages/Specialties';
-import Doctors from './pages/Doctors';
-import GotoCommunity from './pages/GotoCommunity';
-import HealthQuestions from './pages/HealthQuestions';
-import Help from './pages/Help';
-import Reviews from './pages/Reviews';
-import HexaNews from './pages/HexaNews';
-import PatientsStories from './pages/PatientsStories';
-import LatestArticles from './pages/LatestArticles';
-import LatestVideos from './pages/LatestVideos';
-import Booking from './pages/Booking';
-import Locations from './pages/Locations';
-import Faq from './pages/Faq';
-import MobileFooter from './components/MobileFooter';
-import Chatbot from './pages/Chatbot';
-import AreaPage from './pages/AreaPage';
-import Footer from './pages/Footer';
+import Navbar from './components/Navbar.jsx'
+import Herobar from './components/Herobar.jsx'
+import Quotes from './pages/Quotes.jsx'
+import Hospitals from './pages/Hospitals.jsx'
+import Solutions from './pages/Solutions.jsx'
+import HealthGPT from './pages/HealthGPT.jsx'
+import Specialties from './pages/Specialties.jsx'
+import Doctors from './pages/Doctors.jsx'
+import GotoCommunity from './pages/GotoCommunity.jsx'
+import HealthQuestions from './pages/HealthQuestions.jsx'
+import Help from './pages/Help.jsx'
+import Reviews from './pages/Reviews.jsx'
+import HexaNews from './pages/HexaNews.jsx'
+import PatientsStories from './pages/PatientsStories.jsx'
+import LatestArticles from './pages/LatestArticles.jsx'
+import LatestVideos from './pages/LatestVideos.jsx'
+import Booking from './pages/Booking.jsx'
+import Locations from './pages/Locations.jsx'
+import Faq from './pages/Faq.jsx'
+import MobileFooter from './components/MobileFooter.jsx'
+import Chatbot from './pages/Chatbot.jsx'
+import AreaPage from './pages/AreaPage.jsx'
+import Footer from './pages/Footer.jsx'
+
 import { Routes, Route } from 'react-router-dom';
+
+
 export default function App() {
     return (
         <Routes>
